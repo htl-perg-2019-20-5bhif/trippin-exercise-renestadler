ac75a5dce6a47c805adaa2af5f482044ed32c6a2
renestadler <stadler.rene2000@gmail.com>
2019-10-10 23:35:34 +0200

    Final Version

---
+++ Trippin_Exercise/Dto.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Trippin_Exercise
+{
+    public class PersonInputDto
+    {
+
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string CityName { get; set; }
+
+        public string Country { get; set; }
+    }
+
+    public class PersonOutputDto
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IEnumerable<string> Emails { get; set; }
+        public IEnumerable<AddressInfo> AddressInfo { get; set; }
+
+        public PersonOutputDto(PersonInputDto personInputDto)
+        {
+            this.UserName = personInputDto.UserName;
+            this.FirstName = personInputDto.FirstName;
+            this.LastName = personInputDto.LastName;
+            Emails = Enumerable.Empty<string>().Append(personInputDto.Email);
+            AddressInfo addressInfo = new AddressInfo();
+            addressInfo.Address = personInputDto.Address;
+            addressInfo.City = new City();
+            addressInfo.City.Name = personInputDto.CityName;
+            addressInfo.City.CountryRegion = personInputDto.Country;
+            AddressInfo = Enumerable.Empty<AddressInfo>().Append(addressInfo);
+        }
+    }
+
+    public class AddressInfo
+    {
+        public string Address { get; set; }
+        public City City { get; set; }
+    }
+
+    public class City
+    {
+        public string Name { get; set; }
+        public string CountryRegion { get; set; }
+        public string Region { get; set; }
+    }
+}
---
+++ Trippin_Exercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Trippin_Exercise
+{
+    class Program
+    {
+        static HttpClient HttpClient = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            HttpClient.BaseAddress = new Uri("https://services.odata.org/TripPinRESTierService/apiKey/");
+            IEnumerable<PersonInputDto> people = (JsonSerializer.Deserialize<IEnumerable<PersonInputDto>>(await File.ReadAllTextAsync("users.json")));
+            foreach (var item in people)
+            {
+
+                HttpResponseMessage response = await HttpClient.GetAsync("People('" + item.UserName + "')");
+                if (!response.IsSuccessStatusCode)
+                {
+                    var content = new StringContent(JsonSerializer.Serialize(new PersonOutputDto(item)), Encoding.UTF8, "application/json");
+                    await HttpClient.PostAsync("People", content);
+                }
+            }
+        }
+    }
+}
